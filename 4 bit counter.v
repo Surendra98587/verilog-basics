d74ccfd20685cd66ea69f0a0fa4ea277dd1b584d
Vlsi student <talarisurendra98@gmail.com>
2026-06-15 18:21:27 +0530

    Update 4 bit counter.v

--- 4 bit counter.v
+++ 4 bit counter.v
@@ -15,3 +15,24 @@ else
 count<=count-1;
 end
 endmodule 
+
+here we will use mode
+if reset is 1 
+count<=4'b0000;
+
+if reset is 0 mode 1 upcounter
+count<=count+1;
+
+if reset is 0 mode0 down counter
+count<=count-1;
+
+module up_down_counter(input clk, input reset,input mode, output reg [3:0] count);
+always@(posedge clk)begin
+if(reset)
+count<=4'b0000;
+else if(mode)
+count<=count+1;
+else
+count<=count-1;
+end
+endmodule 
